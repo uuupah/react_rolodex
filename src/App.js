fc6befc218f263873002539271e5c86e4e3f9ddf
uuupah <uuupah@gmail.com>
2020-02-12 13:32:03 +1030

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: "Frankenstein",
+          id: "0001"
+        },
+        {
+          name: "Dracula",
+          id: "0002"
+        },
+        {
+          name: "Zombie",
+          id: "0003"
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {// .map iterates across an array
+        this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
+
+/*
+ * weird watcher bug was solved using
+ *
+ * // I have had this error too. Try
+ * //
+ * // $ echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf
+ * // and
+ * // $ sudo sysctl -p
+ * //
+ * // This document helped me
+ * // https://github.com/guard/listen/wiki/Increasing-the-amount-of-inotify-watchers#the-technical-details
+ *
+ * from
+ * https://github.com/facebook/create-react-app/issues/7612
+ *
+ * this seems like a really ugly and potentially dangerous fix, but it is a fix
+ */
